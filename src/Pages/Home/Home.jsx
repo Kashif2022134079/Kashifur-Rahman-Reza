06d02e990608a1167e19afeda917cba6fc38c070
Kashif <krreza200@gmail.com>
2025-01-17 20:20:27 +0600

    Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 
 const Home = () => {
     return (
-        <div>
+        <div className="mb-4">
             <h2 className="text-2xl font-bold mb-3 text-gray-600">About Me</h2>
             <p className="md:text-lg text-base text-gray-600 mb-3">
                 Hello! My name is Kashifur Rahman Reza, currently pursuing my undergraduate degree in Statistics at Shahjalal University of Science and Technology.
@@ -9,12 +9,13 @@ const Home = () => {
                 HTML, CSS, JavaScript, React.js, Node.js, Express.js,</span> and <span className="font-medium">MongoDB</span>. My journey into web development began with a keen interest in how websites are built, which has now blossomed into a comprehensive skill set. 
                 <br /> <br />
                 Recently, I developed a full-stack e-commerce website for a friends clothing business, Asrar Fashion, enhancing their online presence and streamlining the shopping experience for customers. This project allowed me to apply my skills in web development, e-commerce, and project management.
-                You can check my <a className="text-blue-600" href="https://www.linkedin.com/in/kashif79/">LinkedIn profile</a> for details regarding my past professional experiences.
+                You can check my <a className="text-blue-600" href="https://www.linkedin.com/in/kashif79/">LinkedIn profile</a> for more about my journey and the projects I have worked on so far.
                 <br /> <br />
                 Beyond work, I lead a rather uneventful life: I cook food, learning recipes from my mom to stave off the impending doom of hunger, enjoy the thrill of cricket matches, and attempt to stroke my fragile ego by mastering the ball on the field.
                 <br />
                 <br />I am currently seeking an entry-level web developer position where I can apply my skills and passion for technology, and I am excited about the opportunity to contribute to a dynamic team. Feel free to reach out to me at 
-                +8801877273449 or check out my <a href="file:///C:/Users/SONY/Downloads/Kashifur%20Rahman%20Reza-Resume.pdf" className="text-blue-600">Resume</a>.
+                +8801877273449 or check out my <a href="/public/Kashifur Rahman Reza- Resume (1).pdf" className="text-blue-600">Resume</a>.
+
             </p>
         </div>
     );
